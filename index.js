077ee6ca76e6ff66a28d9ec8f9ff256cd889109c
vrunoa <vruno.alassia@gmail.com>
2015-07-02 17:37:36 -0300

    add request timeout, if appium doesnt responses after 2000ms return NO

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = function(port, callback) {
 	var port = isNaN(port) ? 4723 : parseInt(port),
 	req = require("request"),
 	appiumURL = "http://127.0.0.1:"+port+"/wd/hub/status";
-	req(appiumURL, function (error, response, body) {
+  req.get({url:appiumURL,timeout: 2000}, function (error, response, body) {
 		if(error || response.statusCode != 200) {
 			callback(false);
 			return;
